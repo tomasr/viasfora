1bb2c16a5b8acdbb88fad1221e4ed75f80018058
Tomas Restrepo <tomas@winterdom.com>
2013-10-17 21:55:49 -0500

    Refactor format definitions

---
+++ Classifications/CodeClassificationDefinitions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Winterdom.Viasfora.Classifications {
+
+  internal static class CodeClassificationDefinitions {
+    [Export, Name(Constants.KEYWORD_CLASSIF_NAME)]
+    internal static ClassificationTypeDefinition FlowControlClassificationType = null;
+
+    [Export, Name(Constants.LINQ_CLASSIF_NAME)]
+    internal static ClassificationTypeDefinition LinqKeywordClassificationType = null;
+
+    [Export, Name(Constants.STRING_ESCAPE_CLASSIF_NAME)]
+    internal static ClassificationTypeDefinition StringEscapeSequenceClassificationType = null;
+
+    [Export, Name(Constants.VISIBILITY_CLASSIF_NAME)]
+    internal static ClassificationTypeDefinition VisibilityKeywordClassificationType = null;
+  }
+}
