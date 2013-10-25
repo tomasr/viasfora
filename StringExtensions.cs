cd9ed4567ec16af5cdafb5363a154a2cb1f8de2f
Tomas Restrepo <tomas@winterdom.com>
2013-10-24 22:26:50 -0500

    Format option value lists better

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -7,12 +7,16 @@ namespace Winterdom.Viasfora {
   static class StringExtensions {
     public static String[] AsList(this String str) {
       if ( String.IsNullOrEmpty(str) ) return null;
-      return str.Split(new Char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+      String[] values = str.Split(new Char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+      for ( int i = 0; i < values.Length; i++ ) {
+        values[i] = values[i].Trim();
+      }
+      return values;
     }
     public static String FromList(this IEnumerable<String> list) {
       StringBuilder sb = new StringBuilder();
       foreach ( String s in list ) {
-        if ( sb.Length > 0 ) sb.Append(',');
+        if ( sb.Length > 0 ) sb.Append(", ");
         sb.Append(s);
       }
       return sb.ToString();
