50693531b8b0668ee06eec1f658367d6c7842f84
Tomas Restrepo <tomas@winterdom.com>
2014-01-03 22:33:53 -0500

    Update check for rainbow tags in keyword classifier

--- Text/KeywordTagger.cs
+++ Text/KeywordTagger.cs
@@ -18,7 +18,6 @@ namespace Winterdom.Viasfora.Text {
     private KeywordTag linqClassification;
     private KeywordTag visClassification;
     private KeywordTag stringEscapeClassification;
-    private IClassificationType[] rainbowTypes;
     private ITagAggregator<IClassificationTag> aggregator;
 
 #pragma warning disable 67
@@ -38,7 +37,6 @@ namespace Winterdom.Viasfora.Text {
          new KeywordTag(registry.GetClassificationType(Constants.VISIBILITY_CLASSIF_NAME));
       stringEscapeClassification =
          new KeywordTag(registry.GetClassificationType(Constants.STRING_ESCAPE_CLASSIF_NAME));
-      rainbowTypes = RainbowTagger.GetRainbows(registry, Constants.MAX_RAINBOW_DEPTH);
 
       VsfSettings.SettingsUpdated += this.OnSettingsUpdated;
       this.aggregator = aggregator;
@@ -57,7 +55,7 @@ namespace Winterdom.Viasfora.Text {
 
       ITextSnapshot snapshot = spans[0].Snapshot;
       foreach ( var tagSpan in aggregator.GetTags(spans) ) {
-        if ( this.rainbowTypes.Contains(tagSpan.Tag.ClassificationType) )
+        if ( tagSpan.Tag.ClassificationType is RainbowTag )
           continue;
         String name = tagSpan.Tag.ClassificationType.Classification.ToLower();
         if ( eshe && name.Contains("string") ) {
