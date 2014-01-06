a5c14745fd8f10f9c569e436c6365e17cb3ea08b
Tomas Restrepo <tomas@winterdom.com>
2014-01-05 21:26:14 -0500

    Add interface for parsing the first line comment in a file and an implementation for C-based languages

---
+++ Util/CBasedFirstLineCommentParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Winterdom.Viasfora.Util {
+  public class CBasedFirstLineCommentParser : IFirstLineCommentParser {
+    public string Parse(ITextChars tc) {
+      while ( !tc.EndOfLine && tc.Char() != '/' ) {
+        tc.Next();
+      }
+      if ( tc.EndOfLine ) return null;
+
+      StringBuilder sb = new StringBuilder();
+      if ( tc.NChar() == '*' ) {
+        // multiline comment
+        while ( !tc.EndOfLine && tc.NChar() != '*' && tc.NNChar() != '/' ) {
+          sb.Append(tc.Char());
+          tc.Next();
+        }
+      } else if ( tc.NChar() == '/' ) {
+        // single line comment
+        while ( !tc.EndOfLine ) {
+          sb.Append(tc.Char());
+          tc.Next();
+        }
+      }
+      return sb.ToString(); 
+    }
+  }
+}
