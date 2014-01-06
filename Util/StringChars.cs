648ba339680216918270e5066d4312461f158c64
Tomas Restrepo <tomas@winterdom.com>
2014-01-05 21:26:57 -0500

    Implement ITextChars for a piece of text, and update LineChars to derive from it

---
+++ Util/StringChars.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Winterdom.Viasfora.Util {
+  public class StringChars : ITextChars {
+    private String text;
+    private int position;
+    private int length;
+    const char EOT = '\0';
+    public int Position {
+      get { return position; }
+    }
+
+    public virtual int AbsolutePosition {
+      get { return position; }
+    }
+
+    public bool EndOfLine {
+      get { return position >= length; }
+    }
+
+    public StringChars(String text, int start=0) {
+      this.text = text;
+      this.length = text.Length;
+      this.position = start;
+    }
+
+    public char Char() {
+      return Available(1) ? text[position] : EOT;
+    }
+
+    public char NChar() {
+      return Available(2) ? text[position+1] : EOT;
+    }
+
+    public char NNChar() {
+      return Available(3) ? text[position+2] : EOT;
+    }
+
+    public void Next() {
+      Skip(1);
+    }
+
+    public void Skip(int count) {
+      this.position += count;
+    }
+
+    public void SkipRemainder() {
+      this.position = this.length;
+    }
+
+    public string PreviousToken() {
+      int startPos = this.Position-1;
+      // skip any whitespace
+      for ( ; startPos > 0; startPos-- ) {
+        if ( !System.Char.IsWhiteSpace(text[startPos]) )
+          break;
+      }
+      int end = startPos;
+      for ( ; startPos > 0; startPos-- ) {
+        if ( System.Char.IsWhiteSpace(text[startPos]) )
+          break;
+      }
+      if ( startPos < 0 ) return "";
+      return text.Substring(startPos, end - startPos + 1);
+    }
+    private bool Available(int count) {
+      return this.position + count - 1 < length;
+    }
+  }
+}
