4151ffa5dfa60d5998b6eddea7216ced89bcd055
Tomas Restrepo <tomas@winterdom.com>
2014-01-12 22:16:15 -0500

    Adding GenericCommentParser class and associated test

---
+++ Viasfora.Tests/CommentParsers/GenericCommentParserTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Winterdom.Viasfora.Languages.CommentParsers;
+using Winterdom.Viasfora.Util;
+using Xunit;
+
+namespace Viasfora.Tests.CommentParsers {
+  public class GenericCommentParserTests {
+
+    [Fact]
+    public void CBasedSingleLineComment() {
+      const String input = "// vim: ts=4:sw=8";
+      var parser = new GenericCommentParser();
+      String result = parser.Parse(new StringChars(input));
+      Assert.Equal("vim: ts=4;sw=8", result);
+    }
+
+  }
+}
---
+++ Viasfora/Languages/CommentParsers/GenericCommentParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Winterdom.Viasfora.Util;
+
+namespace Winterdom.Viasfora.Languages.CommentParsers {
+  // Generic, single-line comment parser
+  // that takes care of the most common cases
+  public class GenericCommentParser : IFirstLineCommentParser {
+    public string Parse(ITextChars tc) {
+      throw new NotImplementedException();
+    }
+  }
+}
