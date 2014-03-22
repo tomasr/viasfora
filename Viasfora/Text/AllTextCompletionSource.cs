b299c2d13da9133ae3c63a17dbb967d0b4995f9e
Tomas Restrepo <tomas@winterdom.com>
2014-03-21 21:36:02 -0500

    Add initial stubs for all-text completion source

---
+++ Viasfora/Text/AllTextCompletionSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+
+namespace Winterdom.Viasfora.Text {
+  public class AllTextCompletionSource : ICompletionSource {
+    private ITextBuffer theBuffer;
+    private ITextSearchService textSearch;
+
+    public AllTextCompletionSource(ITextBuffer buffer, ITextSearchService searchService) {
+      this.theBuffer = buffer;
+      this.textSearch = searchService;
+    }
+    public void AugmentCompletionSession(ICompletionSession session, IList<CompletionSet> completionSets) {
+    }
+
+    public void Dispose() {
+    }
+  }
+}
---
+++ Viasfora/Text/AllTextCompletionSourceProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Winterdom.Viasfora.Text {
+  [Export(typeof(ICompletionSourceProvider))]
+  [ContentType("plainText")]
+  [Name("viasfora.text.completion")]
+  public class AllTextCompletionSourceProvider : ICompletionSourceProvider {
+    [Import]
+    private ITextSearchService searchService = null;
+    public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer) {
+      return new AllTextCompletionSource(textBuffer, searchService);
+    }
+  }
+}
