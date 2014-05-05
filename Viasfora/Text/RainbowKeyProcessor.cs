35d3469c82b01dddef76e4daf3f37d0c6ca0241f
Tomas Restrepo <tomas@winterdom.com>
2014-05-05 05:37:42 -0500

    Adding initial implementation of RainbowKeyProcessor

---
+++ Viasfora/Text/RainbowKeyProcessor.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Winterdom.Viasfora.Text {
+
+  [Export(typeof(IKeyProcessorProvider))]
+  [Name("viasfora.rainbow.key.provider")]
+  [Order(After="Default")]
+  [TextViewRole(PredefinedTextViewRoles.Editable)]
+  [ContentType("text")]
+  public class RainbowKeyProcessorProvider : IKeyProcessorProvider {
+
+    public KeyProcessor GetAssociatedProcessor(IWpfTextView wpfTextView) {
+      return new RainbowKeyProcessor(wpfTextView);
+    }
+  }
+
+  public class RainbowKeyProcessor : KeyProcessor {
+    private ITextView theView;
+    private Stopwatch timer = new Stopwatch();
+    public const long PRESS_TIME = 500;
+    public RainbowKeyProcessor(ITextView textView) {
+      this.theView = textView;
+    }
+
+    public override void KeyDown(KeyEventArgs args) {
+      if ( args.Key == Key.LeftCtrl ) {
+        if ( timer.IsRunning ) {
+          if ( timer.ElapsedMilliseconds >= PRESS_TIME ) {
+            timer.Stop();
+            StartRainbowAdornment();
+          }
+        } else {
+          timer.Start();
+        }
+      } else {
+        timer.Stop();
+      }
+    }
+
+    public override void KeyUp(KeyEventArgs args) {
+      timer.Stop();
+      StopRainbowAdornment();
+    }
+
+    private void StartRainbowAdornment() {
+      throw new NotImplementedException();
+    }
+
+    private void StopRainbowAdornment() {
+      throw new NotImplementedException();
+    }
+  }
+}
