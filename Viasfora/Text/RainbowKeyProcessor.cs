bbf1bc322de42965ee9f7dbb7d7289f9276c7725
Tomas Restrepo <tomas@winterdom.com>
2014-05-07 17:40:07 -0500

    Reduce the effect start time to 300ms and also avoid starting it again when it already is visible

--- Viasfora/Text/RainbowKeyProcessor.cs
+++ Viasfora/Text/RainbowKeyProcessor.cs
@@ -8,6 +8,8 @@ using System.Windows;
 using System.Windows.Input;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
+using Winterdom.Viasfora.Util;
+using Microsoft.VisualStudio.Text;
 
 namespace Winterdom.Viasfora.Text {
 
@@ -26,7 +28,8 @@ namespace Winterdom.Viasfora.Text {
   public class RainbowKeyProcessor : KeyProcessor {
     private ITextView theView;
     private Stopwatch timer = new Stopwatch();
-    public const long PRESS_TIME = 500;
+    public const long PRESS_TIME = 300;
+    private bool startedEffect = false;
     public RainbowKeyProcessor(ITextView textView) {
       this.theView = textView;
     }
@@ -52,13 +55,26 @@ namespace Winterdom.Viasfora.Text {
     }
 
     private void StartRainbowAdornment() {
+      if ( startedEffect ) return;
+      startedEffect = true;
+      var caret = this.theView.Caret.Position.BufferPosition;
+      var buffer = TextEditor.GetPrimaryBuffer(this.theView);
+      RainbowProvider provider;
+      if ( !buffer.Properties.TryGetProperty(typeof(RainbowProvider), out provider) ) {
+        return;
+      }
+      Tuple<BracePos, BracePos> braces = provider.BraceCache.GetBracesAround(caret);
+      if ( braces == null ) return;
+      SnapshotPoint opening = new SnapshotPoint(caret.Snapshot, braces.Item1.Position);
+      SnapshotPoint closing = new SnapshotPoint(caret.Snapshot, braces.Item2.Position);
       RainbowAdornment adornment = RainbowAdornment.Get(this.theView);
-      adornment.Start();
+      adornment.Start(opening, closing);
     }
 
     private void StopRainbowAdornment() {
       RainbowAdornment adornment = RainbowAdornment.Get(this.theView);
       adornment.Stop();
+      startedEffect = false;
     }
   }
 }
