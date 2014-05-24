88d6c6548e3674d90eff4129c39d64b8aaee8765
Tomas Restrepo <tomas@winterdom.com>
2014-05-24 17:34:06 -0500

    Add Css escape sequence parser tests

---
+++ Viasfora.Tests/Sequences/CssEscapeSequenceParserTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.Text;
+using Xunit;
+using Winterdom.Viasfora.Languages.Sequences;
+
+namespace Viasfora.Tests.Sequences {
+  public class CssEscapeSequenceParserTests {
+    [Fact]
+    public void NoEscapesReturnsNull() {
+      String input = "'some string'";
+      var parser = new CssEscapeSequenceParser(input);
+      Assert.Equal(null, parser.Next());
+    }
+    [Fact]
+    public void SimpleEscapeSequenceIsExtracted() {
+      String input = @"'some\rstring'";
+      var parser = new CssEscapeSequenceParser(input);
+      Assert.Equal(new Span(5,2), parser.Next());
+      Assert.Equal(null, parser.Next());
+    }
+  }
+}
