00a0335a144f2c99dd4a2268cbe326a257832bf3
Tomas Restrepo <tomas@winterdom.com>
2014-05-24 16:52:02 -0500

    Css brace extraction practically complete

--- Viasfora.Tests/BraceExtractors/CssBraceExtractorTests.cs
+++ Viasfora.Tests/BraceExtractors/CssBraceExtractorTests.cs
@@ -17,9 +17,102 @@ canvas,
 video {
   display: inline-block;
 }";
-      var extractor = new CssBraceExtractor(new PowerShell());
+      var extractor = new CssBraceExtractor(new Css());
       var chars = Extract(extractor, input.Trim(), 0, 0);
       Assert.Equal(2, chars.Count);
+    }
+    [Fact]
+    public void ComplexRule() {
+      String input = @"
+.btn-group > .btn + .dropdown-toggle {
+  padding-right: 8px;
+  padding-left: 8px;
+}
+";
+      var extractor = new CssBraceExtractor(new Css());
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+    [Fact]
+    public void BracesInCommentsAreIgnored() {
+      String input = @"
+/* this is a comment with (){}[] braces */
+";
+      var extractor = new CssBraceExtractor(new Css());
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(0, chars.Count);
+    }
+    [Fact]
+    public void BracesInMultiLineCommentsAreIgnored() {
+      String input = @"
+/* this is a comment
+with (){}[]
+braces
+*/
+";
+      var extractor = new CssBraceExtractor(new Css());
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(0, chars.Count);
+    }
+    [Fact]
+    public void BracesInSingleQuotedStringsAreIgnored() {
+      String input = @"
+'../fonts/glyphicons-halflings-regular.eot (){}'
+";
+      var extractor = new CssBraceExtractor(new Css());
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(0, chars.Count);
+    }
+    [Fact]
+    public void BracesInDoubleQuotedStringsAreIgnored() {
+      String input = @"
+""../fonts/glyphicons-halflings-regular.eot (){}""
+";
+      var extractor = new CssBraceExtractor(new Css());
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(0, chars.Count);
+    }
+    [Fact]
+    public void CanHandleEscapeSequencesInSingleQuotedStrings() {
+      String input = @"
+'\123eab../fonts/glyphicons-halflings-regular.eot\'(){}\''
+";
+      var extractor = new CssBraceExtractor(new Css());
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(0, chars.Count);
+    }
+    [Fact]
+    public void CanHandleEscapeSequencesInDoubleQuotedStrings() {
+      String input = @"
+""\123eab../fonts/glyphicons-halflings-regular.eot\'(){}\'""
+";
+      var extractor = new CssBraceExtractor(new Css());
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(0, chars.Count);
+    }
+    [Fact]
+    public void BracesInMultilineStringsAreIgnored() {
+      String input = @"
+'../fonts/glyphicons-halflings-regular.eot \
+some other string (){} \
+with more stuff'
+";
+      var extractor = new CssBraceExtractor(new Css());
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(0, chars.Count);
+    }
+    [Fact]
+    public void CompleteComplexRule1() {
+      String input = @"
+@font-face {
+  font-family: 'Glyphicons Halflings';
+  src: url('../fonts/glyphicons-halflings-regular.eot');
+  src: url('../fonts/glyphicons-halflings-regular.eot?#iefix') format('embedded-opentype'), url('../fonts/glyphicons-halflings-regular.woff') format('woff'), url('../fonts/glyphicons-halflings-regular.ttf') format('truetype'), url('../fonts/glyphicons-halflings-regular.svg#glyphicons-halflingsregular') format('svg');
+}
+";
+      var extractor = new CssBraceExtractor(new Css());
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2+2+8*2, chars.Count);
     }
 
     private IList<CharPos> Extract(IBraceExtractor extractor, string input, int start, int state) {
--- Viasfora/Languages/BraceExtractors/CssBraceExtractor.cs
+++ Viasfora/Languages/BraceExtractors/CssBraceExtractor.cs
@@ -25,6 +25,9 @@ namespace Winterdom.Viasfora.Languages.BraceExtractors {
     public IEnumerable<CharPos> Extract(ITextChars tc) {
       while ( !tc.EndOfLine ) {
         switch ( this.state ) {
+          case stComment: ParseComment(tc); break;
+          case stSingleQuotedString: ParseString(tc); break;
+          case stDoubleQuotedString: ParseDString(tc); break;
           default:
             foreach ( var ch in ParseText(tc) ) {
               yield return ch;
@@ -36,14 +39,56 @@ namespace Winterdom.Viasfora.Languages.BraceExtractors {
 
     private IEnumerable<CharPos> ParseText(ITextChars tc) {
       while ( !tc.EndOfLine ) {
-        if ( lang.BraceList.Contains(tc.Char()) ) {
+        if ( tc.Char() == '/' && tc.NChar() == '*' ) {
+          this.state = stComment;
+          tc.Skip(2);
+          ParseComment(tc);
+        } else if ( tc.Char() == '"' ) {
+          this.state = stDoubleQuotedString;
+          tc.Next();
+          ParseDString(tc);
+        } else if ( tc.Char() == '\'' ) {
+          this.state = stSingleQuotedString;
+          tc.Next();
+          ParseString(tc);
+        } else if ( lang.BraceList.Contains(tc.Char()) ) {
           yield return new CharPos(tc.Char(), tc.AbsolutePosition);
           tc.Next();
         } else {
           tc.Next();
         }
       }
-     
     }
+    private void ParseString(ITextChars tc) {
+      ParseString(tc, '\'');
+    }
+    private void ParseDString(ITextChars tc) {
+      ParseString(tc, '"');
+    }
+    private void ParseString(ITextChars tc, char quote) {
+      while ( !tc.EndOfLine ) {
+        if ( tc.Char() == '\\' ) {
+          // escape sequence
+          // could be 1-6 hex digits or something else
+          tc.Skip(2);
+        } else if ( tc.Char() == quote ) {
+          tc.Next();
+          this.state = stText;
+          break;
+        }
+        tc.Next();
+      }
+    }
+    private void ParseComment(ITextChars tc) {
+      while ( !tc.EndOfLine ) {
+        if ( tc.Char() == '*' && tc.NChar() == '/' ) {
+          tc.Skip(2);
+          this.state = stText;
+          return;
+        }
+        tc.Next();
+      }
+    }
+
   }
 }
