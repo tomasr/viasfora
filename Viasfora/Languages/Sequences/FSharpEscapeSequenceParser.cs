ccaa660f8739b1b83b71d0ef40ada17a730eaef9
Tomas Restrepo <tomas@winterdom.com>
2014-07-11 23:37:25 -0500

    Rewrote the F# escape sequence parser and added new tests to it (Issue #78)

--- Viasfora/Languages/Sequences/FSharpEscapeSequenceParser.cs
+++ Viasfora/Languages/Sequences/FSharpEscapeSequenceParser.cs
@@ -2,13 +2,95 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.VisualStudio.Text;
+using Winterdom.Viasfora.Util;
 
 namespace Winterdom.Viasfora.Languages.Sequences {
-  public class FSharpEscapeSequenceParser : CEscapeSequenceParser {
-    public FSharpEscapeSequenceParser(String text) : base(text) {
-      if ( text.StartsWith("\"\"\"") ) {
-        this.SetStart(text.Length);
+  public class FSharpEscapeSequenceParser : IEscapeSequenceParser {
+    private ITextChars text;
+    private const String escapeChar = "\"\\'ntbrafv";
+
+    public FSharpEscapeSequenceParser(String text) {
+      this.text = new StringChars(text);
+      // always skip the first char
+      // (since quotes are included in the string)
+      this.text.Next();
+      // If this is an at-string, or a C preprocessor include
+      // skip it
+      if ( text.StartsWith("@") || text.StartsWith("<") ) {
+        this.text.SkipRemainder();
       }
+
+      if ( text.StartsWith("\"\"\"") ) {
+        this.text.SkipRemainder();
+      }
+    }
+    public Span? Next() {
+      while ( !text.EndOfLine ) {
+        if ( text.Char() == '\\' ) {
+          text.Next();
+          if ( escapeChar.IndexOf(text.Char()) >= 0 ) {
+            text.Next();
+            return new Span(text.Position - 2, 2);
+          }
+          if ( Char.IsDigit(text.Char()) && Char.IsDigit(text.NChar()) && Char.IsDigit(text.NNChar()) ) {
+            // a trigraph
+            text.Skip(3);
+            return new Span(text.Position-4, 4);
+          }
+          if ( text.Char() == '0' && !Char.IsDigit(text.NChar()) ) {
+            // \0
+            text.Next();
+            return new Span(text.Position-2, 2);
+          }
+          if ( text.Char() == 'u' ) {
+            text.Next();
+            text.Mark();
+            Span? span = TryParseShortUnicode();
+            if ( span.HasValue ) {
+              return span.Value;
+            }
+            text.BackToMark();
+          }
+          if ( text.Char() == 'U' ) {
+            text.Next();
+            text.Mark();
+            Span? span = TryParseLongUnicode();
+            if ( span.HasValue ) {
+              return span.Value;
+            }
+            text.BackToMark();
+          }
+        } else {
+          text.Next();
+        }
+      }
+      return null;
+    }
+
+    private Span? TryParseShortUnicode() {
+      // \Uhhhhhhhh
+      for ( int i = 0; i < 4; i++ ) {
+        if ( !IsHexDigit(text.Char()) ) {
+          return null;
+        }
+        text.Next();
+      }
+      return new Span(text.Position - 6, 6);
+    }
+    private Span? TryParseLongUnicode() {
+      // \Uhhhhhhhh
+      for ( int i = 0; i < 8; i++ ) {
+        if ( !IsHexDigit(text.Char()) ) {
+          return null;
+        }
+        text.Next();
+      }
+      return new Span(text.Position - 10, 10);
+    }
+    private bool IsHexDigit(char c) {
+      if ( Char.IsDigit(c) ) return true;
+      return (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
     }
   }
 }
