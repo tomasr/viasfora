9fad69707c613fd9474babd00cef1cec40f37d92
Tomas Restrepo <tomas@winterdom.com>
2014-07-14 05:51:53 -0500

    Organize using statements

--- Viasfora/Rainbow/RainbowTipHighlight.cs
+++ Viasfora/Rainbow/RainbowTipHighlight.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.VisualStudio.Text;
-using Microsoft.VisualStudio.Text.Classification;
-using Microsoft.VisualStudio.Text.Editor;
-using Microsoft.VisualStudio.Text.Tagging;
-using Microsoft.VisualStudio.Utilities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
@@ -12,6 +7,11 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
 using Winterdom.Viasfora.Tags;
 using Winterdom.Viasfora.Util;
 
