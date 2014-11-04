28f3be06eaee23201afc0ea4308f0196246dae9a
Tomas Restrepo <tomas@winterdom.com>
2014-11-03 21:19:02 -0500

    Porting some functionality from SelectionOutliningCommand to SelectionOutliningManager and adding tests for it

--- Viasfora.Tests/Outlining/SelectionOutliningManagerTests.cs
+++ Viasfora.Tests/Outlining/SelectionOutliningManagerTests.cs
@@ -50,6 +50,90 @@ namespace Viasfora.Tests.Outlining {
       Assert.Equal(span1, spanRaisedByEvent.Value);
     }
 
+    // test that it creates the regions correctly when the
+    // selection spans complete text lines
+    [Fact]
+    public void CreateRegionsAround_FullLines() {
+      var textBuffer = GetCSharpTextBuffer("Outlining1.txt");
+      var snapshot = textBuffer.CurrentSnapshot;
+      var outlining = SelectionOutliningManager.Get(textBuffer);
+
+      var selectionSpan = GetSpanFromLines(snapshot, 9, 15);
+      outlining.CreateRegionsAround(selectionSpan);
+
+      // validate GetTags() gives us two spans
+      var allDocument = new NormalizedSnapshotSpanCollection(snapshot.GetSpan());
+      var tags = outlining.GetTags(allDocument).ToList();
+
+      var span1 = GetSpanFromLines(snapshot, 0, 8);
+      Assert.Equal(span1, tags.First());
+
+      var span2 = GetSpanFromLines(snapshot, 16, snapshot.LineCount-1);
+      Assert.Equal(span2, tags.Last());
+    }
+
+    // test that it creates the regions correctly when the
+    // selection spans partial text lines
+    [Fact]
+    public void CreateRegionsAround_PartialLines() {
+      var textBuffer = GetCSharpTextBuffer("Outlining1.txt");
+      var snapshot = textBuffer.CurrentSnapshot;
+      var outlining = SelectionOutliningManager.Get(textBuffer);
+
+      var selectionSpan = GetSpanFromLines(snapshot, 9, 15);
+      selectionSpan = new SnapshotSpan(selectionSpan.Start + 10, selectionSpan.End - 2);
+      outlining.CreateRegionsAround(selectionSpan);
+
+      // validate GetTags() gives us two spans
+      var allDocument = new NormalizedSnapshotSpanCollection(snapshot.GetSpan());
+      var tags = outlining.GetTags(allDocument).ToList();
+
+      var span1 = GetSpanFromLines(snapshot, 0, 8);
+      Assert.Equal(span1, tags.First());
+
+      var span2 = GetSpanFromLines(snapshot, 16, snapshot.LineCount-1);
+      Assert.Equal(span2, tags.Last());
+    }
+
+    // test that it creates the regions correctly when the
+    // selection starts at the beginning of the document
+    [Fact]
+    public void CreateRegionsAround_SelectionAtStartOfDocument() {
+      var textBuffer = GetCSharpTextBuffer("Outlining1.txt");
+      var snapshot = textBuffer.CurrentSnapshot;
+      var outlining = SelectionOutliningManager.Get(textBuffer);
+
+      var selectionSpan = GetSpanFromLines(snapshot, 0, 15);
+      outlining.CreateRegionsAround(selectionSpan);
+
+      // validate GetTags() gives us two spans
+      var allDocument = new NormalizedSnapshotSpanCollection(snapshot.GetSpan());
+      var tags = outlining.GetTags(allDocument).ToList();
+
+      var span1 = GetSpanFromLines(snapshot, 16, snapshot.LineCount-1);
+      Assert.Equal(span1, tags.First());
+    }
+
+    // test that it creates the regions correctly when the
+    // selection ends at the end of the document
+    [Fact]
+    public void CreateRegionsAround_SelectionAtEndOfDocument() {
+      var textBuffer = GetCSharpTextBuffer("Outlining1.txt");
+      var snapshot = textBuffer.CurrentSnapshot;
+      var outlining = SelectionOutliningManager.Get(textBuffer);
+
+      int lineCount = snapshot.LineCount;
+      var selectionSpan = GetSpanFromLines(snapshot, lineCount-10, lineCount-1);
+      outlining.CreateRegionsAround(selectionSpan);
+
+      // validate GetTags() gives us two spans
+      var allDocument = new NormalizedSnapshotSpanCollection(snapshot.GetSpan());
+      var tags = outlining.GetTags(allDocument).ToList();
+
+      var span1 = GetSpanFromLines(snapshot, 0, (lineCount - 10)-1);
+      Assert.Equal(span1, tags.First());
+    }
+
     private SnapshotSpan GetSpanFromLines(ITextSnapshot snapshot, int start, int end) {
       var startLine = snapshot.GetLineFromLineNumber(start);
       var endLine = snapshot.GetLineFromLineNumber(end);
