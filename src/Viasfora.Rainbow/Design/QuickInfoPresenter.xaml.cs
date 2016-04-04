a2f725f52e036526420e144b05c807101f5f37f3
Tomas Restrepo <tomas@winterdom.com>
2016-04-04 17:05:10 -0500

    Clean up using statements

--- src/Viasfora.Rainbow/Design/QuickInfoPresenter.xaml.cs
+++ src/Viasfora.Rainbow/Design/QuickInfoPresenter.xaml.cs
@@ -2,16 +2,9 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.VisualStudio.Language.Intellisense;
 using System.Collections.Specialized;
 using Microsoft.VisualStudio.Text.Editor;
