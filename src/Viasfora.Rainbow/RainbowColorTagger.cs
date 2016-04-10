aeb28ebe38ad738f012787800be197177a53472a
Tomas Restrepo <tomas@winterdom.com>
2016-04-09 20:40:38 -0500

    Clean up more using directives

--- src/Viasfora.Rainbow/RainbowColorTagger.cs
+++ src/Viasfora.Rainbow/RainbowColorTagger.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows.Threading;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
