0001373800fc4e90e050014cbe45716c7e4f9d40
Tomas Restrepo <tomas@winterdom.com>
2016-04-18 22:31:50 -0500

    Implement a new classifier type for the XML Prefix highlight when it appears on an closing element tag (Issue #148)

--- src/Viasfora.Xml/XmlTagger.cs
+++ src/Viasfora.Xml/XmlTagger.cs
@@ -12,6 +12,7 @@ namespace Winterdom.Viasfora.Xml {
     private IXmlSettings settings;
     private ClassificationTag xmlCloseTagClassification;
     private ClassificationTag xmlPrefixClassification;
+    private ClassificationTag xmlClosingPrefixClassification;
     private ClassificationTag xmlDelimiterClassification;
     private ClassificationTag razorCloseTagClassification;
     private IMarkupLanguage language;
@@ -34,6 +35,8 @@ namespace Winterdom.Viasfora.Xml {
          new ClassificationTag(registry.GetClassificationType(Constants.XML_CLOSING));
       xmlPrefixClassification =
          new ClassificationTag(registry.GetClassificationType(Constants.XML_PREFIX));
+      xmlClosingPrefixClassification =
+         new ClassificationTag(registry.GetClassificationType(Constants.XML_CLOSING_PREFIX));
       xmlDelimiterClassification =
          new ClassificationTag(registry.GetClassificationType(Constants.DELIMITER));
       razorCloseTagClassification =
@@ -138,8 +141,12 @@ namespace Winterdom.Viasfora.Xml {
         string prefix = text.Substring(0, colon);
         string name = text.Substring(colon + 1);
 
+        var prefixCT = isClosing && settings.XmlCloseTagEnabled
+                     ? xmlClosingPrefixClassification
+                     : xmlPrefixClassification;
         yield return new TagSpan<ClassificationTag>(
-          new SnapshotSpan(cs.Start, prefix.Length), xmlPrefixClassification);
+          new SnapshotSpan(cs.Start, prefix.Length), prefixCT);
+
         yield return new TagSpan<ClassificationTag>(new SnapshotSpan(
           cs.Start.Add(prefix.Length), 1), xmlDelimiterClassification);
 
