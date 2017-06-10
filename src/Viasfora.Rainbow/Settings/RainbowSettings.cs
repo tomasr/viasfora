62c999e8b00c02f802cf6b350c468c5cba22489d
Tomas Restrepo <tomas@winterdom.com>
2017-06-10 20:19:36 +0200

    Refactor setting classes to not depend on VsfSettings

--- src/Viasfora.Rainbow/Settings/RainbowSettings.cs
+++ src/Viasfora.Rainbow/Settings/RainbowSettings.cs
@@ -1,54 +1,52 @@
 ﻿using System;
 using System.ComponentModel.Composition;
+using Winterdom.Viasfora.Settings;
 
 namespace Winterdom.Viasfora.Rainbow.Settings {
   [Export(typeof(IRainbowSettings))]
-  public class RainbowSettings : IRainbowSettings {
-    private IVsfSettings settings;
-
+  public class RainbowSettings : SettingsBase, IRainbowSettings {
     public event EventHandler SettingsChanged;
 
     public int RainbowDepth {
-      get { return settings.GetInt32(nameof(RainbowDepth), 4); }
-      set { settings.SetValue(nameof(RainbowDepth), value); }
+      get { return GetInt32(nameof(RainbowDepth), 4); }
+      set { SetValue(nameof(RainbowDepth), value); }
     }
     public bool RainbowTagsEnabled {
-      get { return settings.GetBoolean(nameof(RainbowTagsEnabled), true); }
-      set { settings.SetValue(nameof(RainbowTagsEnabled), value); }
+      get { return GetBoolean(nameof(RainbowTagsEnabled), true); }
+      set { SetValue(nameof(RainbowTagsEnabled), value); }
     }
     public bool RainbowColorize {
-      get { return settings.GetBoolean(nameof(RainbowColorize), true); }
-      set { settings.SetValue(nameof(RainbowColorize), value); }
+      get { return GetBoolean(nameof(RainbowColorize), true); }
+      set { SetValue(nameof(RainbowColorize), value); }
     }
     public long RainbowCtrlTimer {
-      get { return settings.GetInt64(nameof(RainbowCtrlTimer), 300); }
-      set { settings.SetValue(nameof(RainbowCtrlTimer), value); }
+      get { return GetInt64(nameof(RainbowCtrlTimer), 300); }
+      set { SetValue(nameof(RainbowCtrlTimer), value); }
     }
     public RainbowHighlightMode RainbowHighlightMode {
-      get { return settings.GetEnum(nameof(RainbowHighlightMode), RainbowHighlightMode.TrackNextScope); }
-      set { settings.SetValue(nameof(RainbowHighlightMode), value); }
+      get { return GetEnum(nameof(RainbowHighlightMode), RainbowHighlightMode.TrackNextScope); }
+      set { SetValue(nameof(RainbowHighlightMode), value); }
     }
     public bool RainbowToolTipsEnabled {
-      get { return settings.GetBoolean(nameof(RainbowToolTipsEnabled), true); }
-      set { settings.SetValue(nameof(RainbowToolTipsEnabled), value); }
+      get { return GetBoolean(nameof(RainbowToolTipsEnabled), true); }
+      set { SetValue(nameof(RainbowToolTipsEnabled), value); }
     }
     public RainbowColoringMode RainbowColoringMode {
-      get { return settings.GetEnum(nameof(RainbowColoringMode), RainbowColoringMode.Unified); }
-      set { settings.SetValue(nameof(RainbowColoringMode), value); }
+      get { return GetEnum(nameof(RainbowColoringMode), RainbowColoringMode.Unified); }
+      set { SetValue(nameof(RainbowColoringMode), value); }
     }
 
     [ImportingConstructor]
-    public RainbowSettings(IVsfSettings settings) {
-      this.settings = settings;
-      this.settings.SettingsChanged += OnSettingsChanged;
+    public RainbowSettings(ISettingsStore store, IStorageConversions converter) : base(store, converter) {
+    }
+
+    public void Load() {
+      this.Store.Load();
     }
 
     public void Save() {
-      this.settings.Save();
-    }
-
-    private void OnSettingsChanged(object sender, EventArgs e) {
-      SettingsChanged?.Invoke(this, e);
+      this.Store.Save();
+      SettingsChanged?.Invoke(this, EventArgs.Empty);
     }
   }
 }
--- src/Viasfora.Xml/Settings/XmlSettings.cs
+++ src/Viasfora.Xml/Settings/XmlSettings.cs
@@ -1,39 +1,36 @@
 ﻿using System;
 using System.ComponentModel.Composition;
+using Winterdom.Viasfora.Settings;
 
 namespace Winterdom.Viasfora.Xml.Settings {
   [Export(typeof(IXmlSettings))]
-  public class XmlSettings : IXmlSettings {
-    private IVsfSettings settings;
-
+  public class XmlSettings : SettingsBase, IXmlSettings {
     public event EventHandler SettingsChanged;
 
     public bool XmlnsPrefixEnabled {
-      get { return settings.GetBoolean(nameof(XmlnsPrefixEnabled), true); }
-      set { settings.SetValue(nameof(XmlnsPrefixEnabled), value); }
+      get { return GetBoolean(nameof(XmlnsPrefixEnabled), true); }
+      set { SetValue(nameof(XmlnsPrefixEnabled), value); }
     }
     public bool XmlCloseTagEnabled {
-      get { return settings.GetBoolean(nameof(XmlCloseTagEnabled), true); }
-      set { settings.SetValue(nameof(XmlCloseTagEnabled), value); }
+      get { return GetBoolean(nameof(XmlCloseTagEnabled), true); }
+      set { SetValue(nameof(XmlCloseTagEnabled), value); }
     }
     public bool XmlMatchTagsEnabled {
-      get { return settings.GetBoolean(nameof(XmlMatchTagsEnabled), true); }
-      set { settings.SetValue(nameof(XmlMatchTagsEnabled), value); }
+      get { return GetBoolean(nameof(XmlMatchTagsEnabled), true); }
+      set { SetValue(nameof(XmlMatchTagsEnabled), value); }
     }
 
     [ImportingConstructor]
-    public XmlSettings(IVsfSettings settings) {
-      this.settings = settings;
-      this.settings.SettingsChanged += OnSettingsChanged;
+    public XmlSettings(ISettingsStore store, IStorageConversions converter) : base(store, converter) {
+    }
+
+    public void Load() {
+      this.Store.Load();
     }
 
     public void Save() {
-      this.settings.Save();
+      this.Store.Save();
+      SettingsChanged?.Invoke(this, EventArgs.Empty);
     }
-
-    private void OnSettingsChanged(object sender, EventArgs e) {
-      SettingsChanged?.Invoke(this, e);
-    }
-
   }
 }
