93ba4010948e645420fe5762e91707da972b7aef
Tomas Restrepo <tomas@winterdom.com>
2017-07-20 10:17:42 -0500

    Add initial support for JavaScript template literals (Fixes #203)

---
+++ tests/Viasfora.Tests/BraceScanners/JscriptBraceScannerTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Winterdom.Viasfora.Languages.BraceScanners;
+using Xunit;
+
+namespace Viasfora.Tests.BraceScanners {
+  public class JScriptBraceScannerTests : BaseScannerTests {
+
+    [Fact]
+    public void CanExtractParens() {
+      String input = @"(x*(y+7))";
+      var extractor = new JScriptBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(4, chars.Count);
+    }
+    [Fact]
+    public void CanExtractBrackets() {
+      String input = @"x[y[0]]";
+      var extractor = new JScriptBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(4, chars.Count);
+    }
+    [Fact]
+    public void CanExtractBraces() {
+      String input = @"if ( true ) { }";
+      var extractor = new JScriptBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(4, chars.Count);
+    }
+    [Fact]
+    public void IgnoreBracesInSingleLineComment() {
+      String input = @"
+callF(1);
+// callCommented(2);
+";
+      var extractor = new JScriptBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+    [Fact]
+    public void IgnoreBracesInMultilineComment() {
+      String input = @"
+/* callF(1);
+callCommented2(4);
+*/
+";
+      var extractor = new JScriptBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(0, chars.Count);
+    }
+    [Fact]
+    public void IgnoreBracesInString() {
+      String input = "callF(\"some (string)\")";
+      var extractor = new JScriptBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+    [Fact]
+    public void IgnoreBracesInCharLiteral() {
+      String input = "callF(']')";
+      var extractor = new JScriptBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+
+
+    [Fact]
+    public void TemplateLiteral1() {
+      String input = "`${rootFolder}/vendor`";
+      var extractor = new JScriptBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+    [Fact]
+    public void TemplateLiteralMultiline() {
+      String input = "`${a + b}\r\n ${b + c}`";
+      var extractor = new JScriptBraceScanner();
+      var chars = ExtractWithLines(extractor, input.Trim(), 0, 0);
+      Assert.Equal(4, chars.Count);
+    }
+    [Fact]
+    public void TemplateLiteralEscapedTick() {
+      String input = "`${a + b}\\`{b + c}`";
+      var extractor = new JScriptBraceScanner();
+      var chars = ExtractWithLines(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+    [Fact]
+    public void TemplateLiteralEscapedDollar() {
+      String input = "`${a + b}\\${b + c}`";
+      var extractor = new JScriptBraceScanner();
+      var chars = ExtractWithLines(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+  }
+}
