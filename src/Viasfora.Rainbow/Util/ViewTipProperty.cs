66ffa5f1fb7751125ba0d16eefb92b94074e98a0
Tomas Restrepo <tomas@winterdom.com>
2017-12-02 22:36:02 -0500

    Make ViewTipProperty mutable and avoid adding it twice to an ITextView (Issue #223)

--- src/Viasfora.Rainbow/Util/ViewTipProperty.cs
+++ src/Viasfora.Rainbow/Util/ViewTipProperty.cs
@@ -3,9 +3,8 @@ using System;
 
 namespace Winterdom.Viasfora.Util {
   public class ViewTipProperty {
-    public SnapshotPoint Position { get; private set; }
-    public ViewTipProperty(SnapshotPoint position) {
-      this.Position = position;
+    public SnapshotPoint Position { get; set; }
+    public ViewTipProperty() {
     }
   }
 }
