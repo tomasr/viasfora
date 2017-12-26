daac6d50fc07c8fab2f762bf9f99c66e588c3940
Tomas Restrepo <tomas@winterdom.com>
2017-12-25 21:57:47 -0500

    Initial Fortran support (#229)
    * Initial support for Fortran. Only keyword highlighting enabled.
    
    * Add support for rainbow braces in fortran code
    
    * Add options page for Fortran

---
+++ tests/Viasfora.Tests/BraceScanners/FortranBraceScannerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Winterdom.Viasfora.Languages.BraceScanners;
+using Xunit;
+
+namespace Viasfora.Tests.BraceScanners {
+  public class FortranBraceScannerTests : BaseScannerTests {
+
+    [Fact]
+    public void CanExtractParens() {
+      String input = @"(x*(y+7))";
+      var extractor = new FortranBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(4, chars.Count);
+    }
+
+    [Fact]
+    public void IgnoreBracesInSingleLineComment() {
+      String input = @"
+call F(1);
+! call F(2);
+";
+      var extractor = new FortranBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+
+    [Fact]
+    public void IgnoreBracesInString_SingleQuoted() {
+      String input = "call F('some (string)')";
+      var extractor = new FortranBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+
+    [Fact]
+    public void IgnoreBracesInString_SingleQuoted_Quoted() {
+      String input = "call F('some''ss (string)')";
+      var extractor = new FortranBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+
+    [Fact]
+    public void IgnoreBracesInString_DoubleQuoted_Quoted() {
+      String input = "call F(\"some\"\"ss (string)\")";
+      var extractor = new FortranBraceScanner();
+      var chars = Extract(extractor, input.Trim(), 0, 0);
+      Assert.Equal(2, chars.Count);
+    }
+  }
+}
