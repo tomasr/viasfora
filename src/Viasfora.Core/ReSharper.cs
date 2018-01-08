6fcffc892d89020b3d7a500429633d8a752d4509
Tomas Restrepo <tomas@winterdom.com>
2018-01-07 19:22:06 -0500

    Remove usings not needed

--- src/Viasfora.Core/ReSharper.cs
+++ src/Viasfora.Core/ReSharper.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
