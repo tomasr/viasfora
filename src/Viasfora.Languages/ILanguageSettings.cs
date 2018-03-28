cf629036c5be8989be8ebd7d23379cc5cf31e8d5
Tomas Restrepo <tomas@winterdom.com>
2018-03-28 08:58:09 -0500

    Move language interfaces to Languages assembly

---
+++ src/Viasfora.Languages/ILanguageSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Winterdom.Viasfora.Contracts {
+  public interface ILanguageSettings {
+    String KeyName { get; }
+    String[] ControlFlow { get; set; }
+    String[] Linq { get; set; }
+    String[] Visibility { get; set; }
+    bool Enabled { get; set; }
+    void Load();
+    void Save();
+  }
+}
