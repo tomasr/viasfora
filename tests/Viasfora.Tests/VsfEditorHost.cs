b4011cd2a17e6d3591e4aebd14be973a50071de5
Tomas Restrepo <tomas@winterdom.com>
2021-11-11 20:52:08 -0500

    Made some progress on fixing tests

--- tests/Viasfora.Tests/VsfEditorHost.cs
+++ tests/Viasfora.Tests/VsfEditorHost.cs
@@ -1,15 +1,35 @@
-﻿using EditorUtils;
+﻿using System;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
 using System.ComponentModel.Composition.Hosting;
+using System.IO;
 
 namespace Viasfora.Tests {
-  public class VsfEditorHost : EditorHost {
-    public VsfEditorHost(CompositionContainer compositionContainer)
-      : base(compositionContainer) {
+  public class VsfEditorHost  {
+    public CompositionContainer CompositionContainer { get; }
+    public ITextBufferFactoryService TextBufferFactoryService { get; }
+    public IContentTypeRegistryService ContentTypeRegistryService { get; }
+    public IBufferTagAggregatorFactoryService BufferTagAggregatorFactory { get; }
+
+    public VsfEditorHost(CompositionContainer compositionContainer) {
+      this.CompositionContainer = compositionContainer;
+      this.TextBufferFactoryService = compositionContainer.GetExportedValue<ITextBufferFactoryService>();
+      this.ContentTypeRegistryService = compositionContainer.GetExportedValue<IContentTypeRegistryService>();
+      this.BufferTagAggregatorFactory = compositionContainer.GetExportedValue<IBufferTagAggregatorFactoryService>();
     }
 
-    public IBufferTagAggregatorFactoryService BufferTagAggregatorFactory {
-      get { return this.CompositionContainer.GetExportedValue<IBufferTagAggregatorFactoryService>(); }
+    public IContentType GetOrCreateContentType(String name, params String[] baseTypes) {
+      var ct = this.ContentTypeRegistryService.GetContentType(name);
+      if ( ct != null ) {
+        return ct;
+      }
+      return this.ContentTypeRegistryService.AddContentType(name, baseTypes);
+    }
+
+    public ITextBuffer CreateTextBuffer(IContentType ct, String content) {
+      var reader = new StringReader(content);
+      return TextBufferFactoryService.CreateTextBuffer(content, ct);
     }
   }
 }
